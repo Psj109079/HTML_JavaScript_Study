04c3c213469f67639b520190fe56cf7fc69583ad
Psj <ckckckemfdjdhk@gmail.com>
2023-02-10 15:22:13 +0900

    commit

---
+++ day0209/multifor2.js
@@ -0,0 +1,8 @@
+// 다중 for문 
+for(var i = 1; i <= 3; i++){
+    console.log("hello");
+    for(var j = 1; j <= 4; j++) {
+        console.log(`\t${i} : ${j}`)
+        if(j == 2) break; // 2번째 for문을 빠져나와서 i++로 이동
+    }
+}
